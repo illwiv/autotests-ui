f45b131d1fcc8e2f8c0ae3ab8e65eaf5f2a6e66b
illwiv <115544744+illwiv@users.noreply.github.com>
2025-08-14 07:07:26 +0300

    Логирование взаимодействий со страницей. Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -1,11 +1,17 @@
 from typing import Pattern
 from playwright.sync_api import Page, expect
 import allure
+from tools.logger import get_logger
+
+logger = get_logger("BASE_COMPONENT")
+
 
 class BaseComponent:
     def __init__(self, page: Page):
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f"Checking that current url matches pattern {expected_url.pattern}"):
+        step = f"Checking that current url matches pattern {expected_url.pattern}"
+        with allure.step(step):
+            logger.info(step)
             expect(self.page).to_have_url(expected_url)
--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -2,4 +2,4 @@ from playwright.sync_api import Page, Route
 
 
 def mock_static_resources(page: Page):
-    page.route("**/*.{ico,png,jpg,svg,webp,mp3,mp4,gif,woff,woff2,ping}", lambda route: route.abort)
+    page.route("**/*.{ico,png,jpg,svg,webp,mp3,mp4,gif,woff,woff2,ping}", lambda route: route.abort())
